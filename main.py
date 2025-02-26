f61b78529410b987a304284c1f940f1775d72549
DanilOrtiz <danilorlandortiz@gmail.com>
2025-02-26 08:17:56 -0600

    Despliegue

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from pydantic import BaseModel
 from bd.database import engine, Base
 from routers.movie import routerMovie
 from routers.users import login_user
+import os
 
 app = FastAPI(
     title = 'Aprendiendo FastAPi',
@@ -21,3 +22,10 @@ Base.metadata.create_all(bind=engine)
 @app.get('/', tags=['inicio'])
 def read_root():
     return HTMLResponse('<h2> Hola mundo! </h2>')
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
+
+
